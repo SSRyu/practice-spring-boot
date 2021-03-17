7d62f5d3be169b58fde3ac7d49670fadf1975508
SSRyu <jjkztok3@gmail.com>
2021-03-17 20:20:56 +0900

    update HelloController

--- src/main/java/com/willowary/practicespringboot/web/HelloController.java
+++ src/main/java/com/willowary/practicespringboot/web/HelloController.java
@@ -1,7 +1,9 @@
 package com.willowary.practicespringboot.web;
 
 
+import com.willowary.practicespringboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+            @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
