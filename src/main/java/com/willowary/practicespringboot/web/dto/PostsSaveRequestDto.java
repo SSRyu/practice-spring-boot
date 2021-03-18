8d4ce705acf55ec2eccf8764fb4c64a895941b81
SSRyu <jjkztok3@gmail.com>
2021-03-18 21:58:37 +0900

    Create PostsSaveRequestDto.java

---
+++ src/main/java/com/willowary/practicespringboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,30 @@
+package com.willowary.practicespringboot.web.dto;
+
+import com.willowary.practicespringboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
