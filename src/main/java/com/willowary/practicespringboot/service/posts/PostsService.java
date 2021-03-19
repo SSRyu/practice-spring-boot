2d49f91b6ad0e6a49026ca8041fef38ab536a741
SSRyu <jjkztok3@gmail.com>
2021-03-19 19:09:49 +0900

    add posts update method

--- src/main/java/com/willowary/practicespringboot/service/posts/PostsService.java
+++ src/main/java/com/willowary/practicespringboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.willowary.practicespringboot.service.posts;
 
+import com.willowary.practicespringboot.domain.posts.Posts;
 import com.willowary.practicespringboot.domain.posts.PostsRepository;
+import com.willowary.practicespringboot.web.dto.PostsResponseDto;
 import com.willowary.practicespringboot.web.dto.PostsSaveRequestDto;
+import com.willowary.practicespringboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,24 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("there is no user. id=" + id)
+        );
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("there is no user. id=" + id)
+        );
+
+        return new PostsResponseDto(entity);
     }
 }
