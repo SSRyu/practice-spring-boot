eb7655a946e92c0f09f567bce1bc8a051744c862
SSRyu <jjkztok3@gmail.com>
2021-03-31 20:54:44 +0900

    SessionUser annotation

--- src/main/java/com/willowary/practicespringboot/web/IndexController.java
+++ src/main/java/com/willowary/practicespringboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.willowary.practicespringboot.web;
 
+import com.willowary.practicespringboot.config.auth.LoginUser;
 import com.willowary.practicespringboot.config.auth.dto.SessionUser;
 import com.willowary.practicespringboot.domain.user.User;
 import com.willowary.practicespringboot.service.posts.PostsService;
@@ -20,10 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
